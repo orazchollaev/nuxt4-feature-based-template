8c21cdb5e1e5002e525776d24f399a2b0ec336c7
Оразнияз Чоллаев <OChollaev@salam.tm.local>
2026-02-08 22:12:59 +0500

    feat: smart feature component auto import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,34 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import setupComponentNaming from "./config/hooks/components";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
   devServer: {
     port: 1010,
     host: "0.0.0.0",
   },
 
   modules: ["@pinia/nuxt"],
+
   components: [
-    {
-      path: "~/components",
-    },
+    { path: "~/components" },
     {
       path: "~/features",
-      pattern: "**/components",
-      prefix: "f",
+      pattern: "*/components/**/*.vue",
       pathPrefix: false,
     },
   ],
 
+  hooks: {
+    "components:extend": setupComponentNaming,
+  },
+
   imports: {
     dirs: [
-      // Global auto-imports
       "composables/**",
       "stores/**",
       "utils/**",
-
-      // Feature-based auto-imports
       "features/*/composables/**",
       "features/*/stores/**",
       "features/*/utils/**",
